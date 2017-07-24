01cd3b12ac1a389817a57d9cbe5056c9b8ecce01
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2017-07-24 16:43:01 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,25 @@
+#Example usage in your profile:
+#  class {'profile::vmtools_win':
+#    use_packages_vmware_com      => false,
+#    local_temp_folder            => 'C:/Windows/Temp',
+#    selfprovided_install_file    => 'VMware-tools-10.1.7-5541682-x86_64.exe',
+#    selfprovided_install_version => '10.1.7.5541682',
+#    selfprovided_file_source     => 'puppet:///filerepo',
+#    minimum_version_level        => 3,
+#  }
+
+# 'minimum_version_level' determines up to which point you want to force upgrades when an existing version is found.
+# when setting this level to 3 with a selfprovided_install_version of '10.1.7.5541682', this module will not upgrade other 10.1.7 builds, only 10.1.6 and lower.
+
 class vmtools_win (
   $use_packages_vmware_com      = false,
   $local_temp_folder            = 'C:/Windows/Temp',
   $selfprovided_install_file    = undef,   #example: 'VMware-tools-10.1.7-5541682-x86_64.exe'
   $selfprovided_install_version = undef,   #example: '10.1.7.5541682'
   $selfprovided_file_source     = undef,   #example: 'puppet:///filerepo'
-  $minimum_major_version        = undef,   #example: 10
-  $minimum_minor_version        = undef,   #example: 1
-  $upgrade_if_same_major_minor  = false,
+  $minimum_version_level        = 3,
 ){
-  if length($facts['vmtools_win_versionmajor']) > 0 {
+  if length($facts['vmtools_win_version']) > 0 {
     #Some version of VMware Tools must be installed...
     info ('An existing version of VMware Tools is currently installed, checking if it needs to be upgraded...')
     if $use_packages_vmware_com {
@@ -16,83 +27,36 @@ class vmtools_win (
       #https://packages.vmware.com/tools/esx/latest/windows/index.html
     }
     else {
+      #Perform validation of self provided package parameters
+      unless ($selfprovided_install_file) and ($selfprovided_install_version) and ($selfprovided_file_source) {
+          fail ('You have to provide values for the $selfprovided_file_source, $selfprovided_install_file and $selfprovided_install_version parameters when you set use_packages_vmware_com to false!')
+      }
+      
       #Check against self-provided VMware Tools installation package
-      if $facts['vmtools_win_versionfull'] == $selfprovided_install_version {
+      $comparison = vmtools_win::compare_version($facts['vmtools_win_version'], $selfprovided_install_version)
+      
+      if has_key($comparison, 'Equal') {
         info ('Installed version is desired version -> no upgrade needed')
         $upgrade_needed = false
       }
+      elsif has_key($comparison, 'Lower') {
+        info ('Installed version is lower version, checking if its below or above the minimum_version_level...')
+        if $comparison['Lower'] <= $minimum_version_level {
+          notify {'Installed version is lower version at or below the minimum_version_level -> upgrade needed':}
+          $upgrade_needed = true
+        }
+        if $comparison['Lower'] > $minimum_version_level {
+          info ('Installed version is lower version but not at the minimum_version_level -> no upgrade needed')
+          $upgrade_needed = false
+        }
+      }
+      elsif has_key($comparison, 'Higher') {
+        info ('Installed version is higher version -> no upgrade needed')
+        $upgrade_needed = false
+      }
       else {
-        info ('Installed version does not match desired version, determining if upgrade is needed...')
-        if ($selfprovided_file_source) and ($selfprovided_install_file) and ($selfprovided_install_version) {
-          #Required parameters are provided, continue processing...
-          if $minimum_major_version {
-            info ('Minimum major version parameter provided, check major version against node...')
-            if (0 + $facts['vmtools_win_versionmajor']) < $minimum_major_version {
-              notify {'Installed VMware Tools is a lower major version vs the required minimum major version -> upgrade needed':}
-              $upgrade_needed = true
-            }
-            elsif (0 + $facts['vmtools_win_versionmajor']) == $minimum_major_version {
-              info ('Installed VMware Tools is the same major version vs the required minimum major version; check minor version...')
-              if $minimum_minor_version {
-                info ('Minimum minor version parameter provided, check against node...')
-                if (0 + $facts['vmtools_win_versionminor']) < $minimum_minor_version {
-                  notify {'Installed VMware Tools is a lower minor version vs the required minimum minor version -> upgrade needed':}
-                  $upgrade_needed = true
-                }
-                elsif (0 + $facts['vmtools_win_versionminor']) == $minimum_minor_version {
-                  #Installed version same major and minor version, but older or newer build; check $upgrade_if_same_major_minor parameter
-                  if $upgrade_if_same_major_minor {
-                    notify {'Installed VMware Tools is the same major and minor version, but a different build. $upgrade_if_same_major_minor is enabled -> upgrade needed':}
-                    $upgrade_needed = true
-                  }
-                  else {
-                    info ('Installed VMware Tools is the same major and minor version, but a different build. $upgrade_if_same_major_minor is disabled -> no upgrade needed')
-                    $upgrade_needed = false
-                  }
-                }
-                elsif (0 + $facts['vmtools_win_versionminor']) > $minimum_minor_version {
-                  info ('Installed version is higher minor version vs the required minimum minor version -> no upgrade needed')
-                  $upgrade_needed = false
-                }
-                else {
-                  fail ('Unable to compare installed minor version against $minimum_minor_version!')
-                }
-              }
-              else {
-                info ('No minimum minor version parameter provided, assuming same minor version and falling back to $upgrade_if_same_major_minor behavior')
-                if $upgrade_if_same_major_minor {
-                  notify {'Installed VMware Tools is the same major and assumed minor version, but a different build. $upgrade_if_same_major_minor is enabled -> upgrade needed':}
-                  $upgrade_needed = true
-                }
-                else {
-                  info ('Installed VMware Tools is the same major and assumed minor version, but a different build. $upgrade_if_same_major_minor is disabled -> no upgrade needed')
-                  $upgrade_needed = false
-                }
-              }
-            }
-            elsif (0 + $facts['vmtools_win_versionmajor']) > $minimum_major_version {
-              info ('Installed version is higher major version vs the required minimum major version -> no upgrade needed')
-              $upgrade_needed = false
-            }
-            else {
-              fail ('Unable to compare installed major version against $minimum_major_version!')
-            }
-          }
-          else {
-            info ('No minimum major version parameter provided, assuming same major and minor version and falling back to $upgrade_if_same_major_minor behavior')
-            if $upgrade_if_same_major_minor {
-              notify {'Installed VMware Tools is assumed the same major and minor version, but a different build. $upgrade_if_same_major_minor is enabled -> upgrade needed':}
-              $upgrade_needed = true
-            }
-            else {
-              info ('Installed VMware Tools is assumed the same major and minor version, but a different build. $upgrade_if_same_major_minor is disabled -> no upgrade needed')
-              $upgrade_needed = false
-            }
-          }
-        }
-        else {
-          fail ('You have to provide values for the $selfprovided_file_source, $selfprovided_install_file and $selfprovided_install_version parameters when you set $use_packages_vmware_com to false!')
-        }
+        info ('Unable to determine version comparison, skipping installation')
+        $upgrade_needed = false
       }
     }
   }
