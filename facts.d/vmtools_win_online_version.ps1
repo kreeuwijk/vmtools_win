f6ddccd649d75c6389350bcb642989b1bb0e96cd
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2017-08-02 13:50:44 +0200

    Added Powershell 2.0 support for Windows 2008 R2
    Powershell 2.0 does not support Invoke-WebRequest, hence an alternative method is required for this older PowerShell version

--- facts.d/vmtools_win_online_version.ps1
+++ facts.d/vmtools_win_online_version.ps1
@@ -1,8 +1,28 @@
-$packagestore = "http://packages.vmware.com/tools/releases/latest/windows/x64"
-$r            = Invoke-WebRequest $packagestore -UseBasicParsing
+$packagestore = "http://packages.vmware.com/tools/releases/latest/windows/x641"
+$success = $false
 
-if ($r.StatusCode -eq 200) {
-    $packageHref    = $r.Links.HREF | ? {$_ -like "*VMware-tools*"}
+if (Get-Command 'Invoke-WebRequest' -ErrorAction SilentlyContinue) {
+    $type = 'IWR'
+    Try {
+       $r = Invoke-WebRequest $packagestore -UseBasicParsing
+    }
+    Catch {}
+    if ($r.StatusCode -eq 200) { $success = $true }
+}
+else {
+    $type = 'WC'
+    Try {
+        $r = (New-Object System.Net.WebClient).DownloadString($packagestore)
+    }
+    Catch {}
+    if ($r) {$success = $true}
+}
+
+if ($success) {
+    Switch ($type) {
+        'IWR' { $packageHref = $r.Links.HREF | ? {$_ -like "*VMware-tools*"} }
+        'WC'  { $packageHref = [regex]::Match($r, '(x64/\D*)(\d*.\d*.\d*-\d*)(-.*exe)').Value }
+    }
     $package        = $packageHref.Split('/')[1]
     $packageversion = [regex]::Match($package, '^(\D*)(\d*.\d*.\d*-\d*)(-.*exe)$').Groups[2].Value -replace "-","."
     $packagelink    = "$package"
