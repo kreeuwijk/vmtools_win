8756012e880b62bec9822a61c2686364e42581ad
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-01-22 17:18:09 +0100

    Update vmtools_win_version.ps1

--- lib/vmtools_win/vmtools_win_version.ps1
+++ lib/vmtools_win/vmtools_win_version.ps1
@@ -1,4 +1,4 @@
 $arrVMtools = @(Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | get-itemproperty | ? {$_.DisplayName -eq "VMware Tools"})
 if ($arrVMtools.Count -gt 0) {
-  write-host "vmtools_win_version=$((Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | get-itemproperty | ? {$_.DisplayName -eq "VMware Tools"} | Sort-Object -Property InstallDate -Descending | Select-Object -First 1 -Property DisplayVersion).DisplayVersion)"
+  write-output (Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | get-itemproperty | ? {$_.DisplayName -eq "VMware Tools"} | Sort-Object -Property InstallDate -Descending | Select-Object -First 1 -Property DisplayVersion).DisplayVersion
 }
