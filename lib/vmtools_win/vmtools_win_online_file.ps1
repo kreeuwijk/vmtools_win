6dc0863a8593eceaa9707d8ed9c165eef3847878
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-01-22 17:14:11 +0100

    Create vmtools_win_online_file.ps1

---
+++ lib/vmtools_win/vmtools_win_online_file.ps1
@@ -0,0 +1,29 @@
+$packagestore = "http://packages.vmware.com/tools/releases/latest/windows/x64"
+$success = $false
+
+if (Get-Command 'Invoke-WebRequest' -ErrorAction SilentlyContinue) {
+    $type = 'IWR'
+    Try {
+       $r = Invoke-WebRequest $packagestore -UseBasicParsing
+    }
+    Catch {}
+    if ($r.StatusCode -eq 200) { $success = $true }
+}
+else {
+    $type = 'WC'
+    Try {
+        $r = (New-Object System.Net.WebClient).DownloadString($packagestore)
+    }
+    Catch {}
+    if ($r) {$success = $true}
+}
+
+if ($success) {
+    Switch ($type) {
+        'IWR' { $packageHref = $r.Links.HREF | ? {$_ -like "*VMware-tools*"} }
+        'WC'  { $packageHref = [regex]::Match($r, '(x64/\D*)(\d*.\d*.\d*-\d*)(-.*exe)').Value }
+    }
+    $package        = $packageHref.Split('/')[1]
+    $packagelink    = "$package"
+    Write-Output "$packagelink"
+}
